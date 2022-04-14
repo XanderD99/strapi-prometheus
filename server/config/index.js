f46a03898556be2317194bbe92327b891ca414a9
XanderD99 <xander.denecker@gmail.com>
2022-04-14 14:39:24 +0200

    :sparkles: add config

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,17 @@
 'use strict';
 
 module.exports = {
-  default: {},
+  default: () => ({
+    defaultMetricsInterval: 10000,
+    durationBuckets: [0.001, 0.005, 0.015, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5],
+    requestSizeBuckets: [5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000],
+    responseSizeBuckets: [5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000],
+    useUniqueHistogramName: false,
+    metricsPrefix: "",
+    excludeRoutes: [],
+    includeQueryParams: false,
+    additionalLabels: [],
+    extractAdditionalLabelValuesFn: undefined
+  }),
   validator() { },
 };
--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -3,7 +3,8 @@
 const { koaMiddleware } = require('prometheus-api-metrics');
 
 module.exports = {
-  metrics: (options, { strapi }) => {
-    return koaMiddleware()
+  metrics: (options) => {
+    options.excludeRoutes.push("/api/metrics");
+    return koaMiddleware(options);
   }
 };
