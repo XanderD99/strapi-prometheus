d6952db5be4b495a30251cd22511790045541e8f
XanderD99 <xander.denecker@gmail.com>
2022-11-04 15:49:25 +0100

    :alien: correctly use prom-client to get single metric

--- server/controllers/prometheus.js
+++ server/controllers/prometheus.js
@@ -14,25 +14,27 @@ module.exports = {
         ctx.body = await register.getMetricsAsJSON();
         break;
       default:
-        ctx.badRequest('Invalid format');
+        ctx.badRequest('Invalid format. possible format: text, json');
     }
   },
   async findOne(ctx) {
     switch (ctx.query.format || 'text') {
       case 'text':
         ctx.response.headers['Content-Type'] = register.contentType;
-        ctx.body = await register.getSingleMetricAsString(ctx.params.name)
+        await register.getSingleMetricAsString(ctx.params.name)
+          .then((metric) => ctx.body = metric)
+          .catch(() => ctx.notFound())
         break;
       case 'json':
         ctx.response.headers['Content-Type'] = 'application/json';
-        ctx.body = await register.getSingleMetric(ctx.params.name);
+        const metrics = await register.getMetricsAsJSON();
+        ctx.body = metrics.find(({ name }) => name === ctx.params.name);
+        if (!ctx.body) {
+          return ctx.notFound()
+        }
         break;
       default:
-        ctx.badRequest('Invalid format');
-    }
-
-    if (!ctx.body) {
-      ctx.notFound()
+        ctx.badRequest('Invalid format. possible format: text, json');
     }
   },
 };
