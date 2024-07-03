6273d0a8267ca96f09232aaf9833ab622e999feb
XanderD99 <xander.denecker@be-mobile.com>
2024-07-03 12:38:50 +0200

    remove dependency on express

--- metrics/metrics.js
+++ metrics/metrics.js
@@ -1,30 +1,33 @@
-module.exports = class MetricServer {
-    app;
-    server;
-    options;
+const http = require('http');
+
+module.exports =  class MetricsServer {
+  constructor(options, register, logger) {
+    this.logger = logger;
+
+    this.server = http.createServer(async (req, res) => {
+        if (req.method === 'GET' && req.url === options.path) {
+          // Replace this with your logic to generate metrics data
+          const data = await register.metrics()
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(data);
+        } else {
+          res.statusCode = 404;
+          res.end('Not Found');
+        }
+      });
   
-    constructor(options, register, logger) {
-        console.log(options)
-        const express = require('express');
-        this.options = options;
-        this.app = express().get(options.path, async (_, response) => {
-            try {
-                const json = await register.metrics()
-                response.setHeader('Content-Type', 'text/plain').status(200).send(json)
-            } catch (error) {
-                response.status(500).send(error)
-            } finally {
-                response.end()
-            }
-        });
+      this.server.listen(options.port, options.host, () => {
+        logger.debug(`Serving metrics on http://${options.host}:${options.port}${options.path}`);
+      });
+  
+  }
 
-        this.server = this.app.listen(this.options.port, this.options.host, () => {
-            logger.info(`Metrics exposed on ${this.options.host}:${this.options.port}${this.options.path || '/metrics'}`);
-        });
-    }
-
-    stop() {
-      this.server?.close();
+  stop() {
+    if (this.server) {
+      this.server.close(() => {
+        this.logger.warn('Metrics server stopped.');
+      });
+      this.server = null;
     }
   }
-  
+}
