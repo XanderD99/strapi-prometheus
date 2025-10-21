ccf16616e0ab51b017e506289ed472d28aaa0560
xander.denecker <36887752+XanderD99@users.noreply.github.com>
2025-10-21 15:37:30 +0200

    feat: remove origin label
    remove origin label from all http related metrics to reduce the amount of data stored.

--- server/src/middlewares/metrics.ts
+++ server/src/middlewares/metrics.ts
@@ -5,7 +5,7 @@ import { NormalizationConfig, PathNormalizationRule } from "src/config";
 const requestDurationSeconds = new Histogram({
   name: 'http_request_duration_seconds',
   help: 'Duration of HTTP requests in seconds',
-  labelNames: ['origin', 'method', 'route', 'status'],
+  labelNames: ['method', 'route', 'status'],
   buckets: [
     0.001, // 1 ms
     0.005, // 5 ms
@@ -24,14 +24,14 @@ const requestDurationSeconds = new Histogram({
 const requestContentLengthBytes = new Histogram({
   name: 'http_request_content_length_bytes',
   help: 'Histogram of the size of payloads sent to the server, measured in bytes.',
-  labelNames: ['origin', 'method', 'route', 'status'],
+  labelNames: ['method', 'route', 'status'],
   buckets: exponentialBuckets(1024, 2, 20) // Buckets starting from 1 KB to 1 GB (1024 * 2^19 = ~536 MB, 2^20 = ~1 GB)
 });
 
 const responseContentLengthBytes = new Histogram({
   name: 'http_response_content_length_bytes',
   help: 'Histogram of the size of payloads sent by the server, measured in bytes.',
-  labelNames: ['origin', 'method', 'route', 'status'],
+  labelNames: ['method', 'route', 'status'],
   buckets: exponentialBuckets(1024, 2, 20) // Buckets starting from 1 KB to 1 GB (1024 * 2^19 = ~536 MB, 2^20 = ~1 GB)
 });
 
@@ -64,7 +64,6 @@ export default async (ctx: Context, next) => {
   const labels = {
     method: ctx.method,
     route,
-    origin: ctx.origin || 'unknown',
     status: ctx.status
   };
 
